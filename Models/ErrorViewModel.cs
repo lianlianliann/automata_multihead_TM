cab263b16bd32a9f9a47f8cb24f91f7f1407fc86
John Rich Nicolas <johnrichnicolas@gmail.com>
2025-11-21 00:37:42 +0800

    First initialization/ creation of project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TM_MULTIHEAD_PHISHING_DETECTOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
