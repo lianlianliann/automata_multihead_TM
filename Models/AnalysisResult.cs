9bf27de385580f8fdb23dc919c295bdef81cd54a
John Rich Nicolas <johnrichnicolas@gmail.com>
2025-12-08 00:29:51 +0800

    created the model for the analyzing results

---
+++ Models/AnalysisResult.cs
@@ -0,0 +1,19 @@
+﻿namespace TM_MULTIHEAD_PHISHING_DETECTOR.Models
+{
+    public class AnalysisResult
+    {
+        public string OriginalText { get; set; }
+        public string Classification { get; set; } // Credible / Suspicious
+        public double ConfidenceScore { get; set; }
+
+        // Per-head data:
+        public List<string> Head1Triggers { get; set; }
+        public List<string> Head2Triggers { get; set; }
+        public List<string> Head3Triggers { get; set; }
+
+        // Scores
+        public double Head1Score { get; set; }
+        public double Head2Score { get; set; }
+        public double Head3Score { get; set; }
+    }
+}
