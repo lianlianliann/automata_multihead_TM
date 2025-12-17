a7c8ffaa966a1d5f2b54492792030081f07ce237
John Rich Nicolas <johnrichnicolas@gmail.com>
2025-12-17 21:21:02 +0800

    ni comment ko lang since diko pa sure logic

--- Models/Head2.cs
+++ Models/Head2.cs
@@ -2,5 +2,19 @@
 {
     public class Head2 //  Structural patterns (excessive punctuation, unusual capitalization)
     {
+
     }
+
+    /*public (double score, List<string> triggers) Run(string text)
+        {
+
+            // DFA-like simulation
+
+            var state = HeadStates.HeadState.q0;
+            var triggers = new List<string>();
+            var words = text.ToCharArray().ispunctuation
+
+            double score = 0.0;
+        }
+    }*/
 }
