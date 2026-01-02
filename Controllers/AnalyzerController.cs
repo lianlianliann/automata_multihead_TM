f25d917f73a4489c2def1e06681cf3db47495b2a
John Rich Nicolas <johnrichnicolas@gmail.com>
2026-01-02 21:35:13 +0800

    fixed the parameter problem

--- Controllers/AnalyzerController.cs
+++ Controllers/AnalyzerController.cs
@@ -8,18 +8,24 @@ namespace TM_MULTIHEAD_PHISHING_DETECTOR.Controllers
 {
     public class AnalyzerController : Controller
     {
-        [HttpPost]
-        public IActionResult Analyze(string postText)
+        [HttpGet]
+        public IActionResult Analyze()
         {
-            if (string.IsNullOrWhiteSpace(postText))
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Analyze(string inputText )
+        {
+            if (string.IsNullOrWhiteSpace(inputText ))
             {
                 return RedirectToAction("Index");
             }
-                
-            var engine = new MHTMEngine();
-            var result = engine.Process(postText);
 
-            return View("Result", result);
+            var engine = new MHTMEngine();
+            var result = engine.Process(inputText );
+
+            return View("~/Views/Home/Result.cshtml", result); // passes AnalysisResult to Result.cshtml
         }
     }
 }
